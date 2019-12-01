d8d4aa44adb850fce3474209945e62c861563df3
hskang <whytul@gmail.com>
2019-12-02 04:20:13 +0900

    수정

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <app-header 
+      v-bind:propsdata="str"
+      v-on:renew="renewStr"></app-header>
+  </div>
+</template>
+
+<script>
+//여기선 컴포넌트 내용을 import 해서 컴포넌트에 등록해서 사용하는게 중요.
+import AppHeader from './components/AppHeader.vue';
+
+// var AppHeader = {
+//   template: '<header><h1>Header</h1></header>'
+// }
+
+// new Vue({
+//   data: {
+//     str: 'hi'
+//   }
+// })
+
+export default {
+  data: function() {
+    return {
+      str: 'Header' //새객체로 리턴한다. 
+    }
+  },
+  components: {
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'hi';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-cli/src/components/a.vue
@@ -0,0 +1,2 @@
+// 특정영역 자바스크립트, css, 템플릿 정의함. sing file componets 라고함.
+// vs에서 vu 입력 후 tab
